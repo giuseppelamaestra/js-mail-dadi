1617269faa8038a3d6bdbb8b55422f6449e80b66
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-05-17 19:11:38 +0200

    Added FOR to the mail exercize but doesn't work

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,6 @@
+
+//DICE EXERCIZE
+
 let randomNumber1 = (Math.floor(Math.random()*6 +1))
 
 let randomNumber2 = (Math.floor(Math.random()*6 +1))
@@ -13,3 +16,30 @@ if(randomNumber1 < randomNumber2){
 } else if (randomNumber2 = randomNumber1) {
    console.log("no one is the winner")
 }
+
+
+
+// MAIL EXERCIZE 
+
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
+//stampa un messaggio appropriato sull’esito del controllo.
+//Tenete presente che non è permesso utilizzare
+ //funzioni built-in degli array come Array.includes() e Array.indexOf(), pena l'annullamento dell'esercizio.
+
+ const Email = prompt("add your e-mail") 
+
+ const EmailList = ["gina@gmail.com", "JudithButler@gmail.com", "PaulPreciado@tiscali.it", "KarlMarx@alice.it", "DrusillaFoer@virgilio.it"]
+
+
+
+ for (let i = 0; i < Email.length; i++){
+    console.log(EmailList[i], i)
+    if(Email != EmailList[i]){
+        console.log("NO")
+    } else {
+        console.log("Si")
+    }
+ }
+
+
+
